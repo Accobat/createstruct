b87762b309a5f149243928efa22461802e8cf0a4
Алексей Подругин <podrugin@hotmail.com>
2020-09-23 14:41:36 +0300

    Updtae2

--- createstruct/Program.cs
+++ createstruct/Program.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 struct Person
 {
-  public string firstName;
+    public enum Genders : int { Male, Female };
+    public string firstName;
   public string lastName;
   public int age;
-  public Person(string _firstName, string _lastName, int _age)
+  public Genders gender;
+    public Person(string _firstName, string _lastName, int _age,Genders _gender)
   {
     firstName = _firstName;
-    lastName = _lastName; age = _age;
-  }
+    lastName = _lastName; age = _age; gender = _gender;
+    }
   public override string ToString()
   {
-    return firstName + " " + lastName + ", age " + age;
-  }
+        return firstName + " " + lastName + " (" + gender + "), age " + age;
+    }
 }
 namespace createstruct
 {
   class Program
   {
-    static void Main(string[] args)
-    {
-      Person p = new Person("Tony", "Allen", 32); Console.WriteLine(p);
+        static void Main(string[] args)
+        {
+            Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+            Console.WriteLine(p.ToString());
+        }
     }
-  }
 }
